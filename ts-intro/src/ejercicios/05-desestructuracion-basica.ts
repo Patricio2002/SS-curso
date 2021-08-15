51781f13eec59cb170ad9c8b6a1841771c80f576
pato <patricio.ad26301@gmail.com>
2021-08-14 20:12:24 -0500

    FEAT: TS

---
+++ ts-intro/src/ejercicios/05-desestructuracion-basica.ts
@@ -0,0 +1,43 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles
+}
+
+interface Detalles{
+    autor: string;
+    anio: number;
+}
+
+const reproductor: Reproductor ={
+    volumen: 90,
+    segundo: 36,
+    cancion: 'Mess',
+    detalles:{
+        autor: 'Ed Sheeran',
+        anio: 2015
+    }
+}
+
+2
+const { volumen, segundo, cancion, detalles } = reproductor;
+const { autor } = detalles
+
+
+//console.log('El volumen actual es de:', volumen)
+//console.log('El segundo actual es de:', segundo)
+//console.log('La cancion actual es de', cancion)
+//console.log('El autor actual es de', autor)
+
+const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+const [ p1, p2, p3 ] =dbz;
+
+console.log('Personaje 1', p1)
+console.log('Personaje 2', p2)
+console.log('Personaje 3', p3)
+
